d1e8843ff38baeda58e8e7be623d76569c991586
Greg1928 <gregoriosortino@protonmail.com>
2022-07-09 18:47:30 +0200

    add header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,1 +1,21 @@
-<header></header>
+<header>
+    <div class="header-top">
+        <div class="container">
+            <span>cd power visa@</span>
+            <span>additional dc sites</span>
+        </div>
+    </div>
+    <div class="header-bottom">
+        <div class="container">
+            <img src="{{ asset('images/dc-logo.png') }}" alt="">
+            <nav class="main-nav">
+                <ul>
+                    @foreach ($links as $link)
+                        <li><a href="{{ $link['url'] }}">{{ $link['text'] }}</a></li>
+                    @endforeach
+                </ul>
+            </nav>
+            <input type="text" placeholder="Search">
+        </div>
+    </div>
+</header>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,57 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    $links = [
+        [
+            'text' => "characters",
+            'url' => "/characters",
+            'current' => false,
+        ],
+        [
+            'text' => "comics",
+            'url' => "/comics",
+            'current' => false,
+        ],
+        [
+            'text' => "movies",
+            'url' => "/movies",
+            'current' => false,
+        ],
+        [
+            'text' => "tv",
+            'url' => "/tv",
+            'current' => false,
+        ],
+        [
+            'text' => "games",
+            'url' => "/games",
+            'current' => false,
+        ],
+        [
+            'text' => "collectibles",
+            'url' => "/collectibles",
+            'current' => false,
+        ],
+        [
+            'text' => "videos",
+            'url' => "/videos",
+            'current' => false,
+        ],
+        [
+            'text' => "fans",
+            'url' => "/fans",
+            'current' => false,
+        ],
+        [
+            'text' => "news",
+            'url' => "/news",
+            'current' => false,
+        ],
+        [
+            'text' => "shops",
+            'url' => "/shops",
+            'current' => false,
+        ],
+    ];
+    return view('home', compact('links'));
 });
