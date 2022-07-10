f38894ec9715e4f4218b646467669678da8b3262
Greg1928 <gregoriosortino@protonmail.com>
2022-07-10 11:55:26 +0200

    add footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,1 +1,78 @@
-<footer></footer>
+<footer>
+    <div class="footer-top">
+        <div class="container">
+            <ul>
+                <li><img class="Png" src="{{ asset('images/buy-comics-digital-comics.png')}}" alt=""><a href="#">digital comics</a></li>
+                <li><img class="Png" src="{{ asset('images/buy-comics-merchandise.png')}}" alt=""><a href="#">dc merchandise</a></li>
+                <li><img class="Png" src="{{ asset('images/buy-comics-subscriptions.png')}}" alt=""><a href="#">subscription</a></li>
+                <li><img class="Png" src="{{ asset('images/buy-comics-shop-locator.png')}}" alt=""><a href="#">comic shop locator</a></li>
+                <li><img src="{{ asset('images/buy-dc-power-visa.svg')}}" alt=""><a href="#">dc power visa</a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="footer-middle">
+        <div class="container">
+            <div class="footer-left">
+                <div class="left-col">
+                    <h3>dc comics</h3>
+                    <ul>
+                        <li><a href="#">link</a></li>
+                        <li><a href="#">link</a></li>
+                        <li><a href="#">link</a></li>
+                        <li><a href="#">link</a></li>
+                        <li><a href="#">link</a></li>
+                        <li><a href="#">link</a></li>
+                        <li><a href="#">link</a></li>
+                    </ul>
+                    <h3>shop</h3>
+                    <ul>
+                        <li><a href="#">link</a></li>
+                        <li><a href="#">link</a></li>
+                    </ul>
+                </div>
+                <div class="middle-col">
+                    <h3>dc</h3>
+                    <ul>
+                        <li><a href="#">link</a></li>
+                        <li><a href="#">link</a></li>
+                        <li><a href="#">link</a></li>
+                        <li><a href="#">link</a></li>
+                        <li><a href="#">link</a></li>
+                        <li><a href="#">link</a></li>
+                        <li><a href="#">link</a></li>
+                        <li><a href="#">link</a></li>
+                        <li><a href="#">link</a></li>
+                        <li><a href="#">link</a></li>
+                        <li><a href="#">link</a></li>
+                    </ul>
+                </div>
+                <div class="right-col">
+                        <h3>sites</h3>
+                    <ul>
+                        <li><a href="#">link</a></li>
+                        <li><a href="#">link</a></li>
+                        <li><a href="#">link</a></li>
+                        <li><a href="#">link</a></li>
+                        <li><a href="#">link</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="footer-right"></div>
+          </div>
+    </div>
+    <div class="footer-bottom">
+        <div class="container">
+            <div class="footer-left">
+                <a href="#">Sign-up Now!</a>
+            </div>
+            <div class="footer-right">
+                <p>follow us</p>
+                <img src="{{ asset('images/footer-facebook.png') }}" alt="">
+                <img src="{{ asset('images/footer-twitter.png') }}" alt="">
+                <img src="{{ asset('images/footer-youtube.png') }}" alt="">
+                <img src="{{ asset('images/footer-periscope.png') }}" alt="">
+                <img src="{{ asset('images/footer-pinterest.png') }}" alt="">
+            </div>
+        </div>
+    </div>
+</footer>
